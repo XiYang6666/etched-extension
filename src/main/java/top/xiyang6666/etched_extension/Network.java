4bb22fa74a556a30944b9563bd07c6499013db2f
XiYang6666 <1782356858@qq.com>
2025-08-11 11:49:33 +0800

    :twisted_rightwards_arrows: sync code

--- src/main/java/top/xiyang6666/etched_extension/Network.java
+++ src/main/java/top/xiyang6666/etched_extension/Network.java
@@ -8,7 +8,7 @@ import top.xiyang6666.etched_extension.packet.EBNRApiPacket;
 public class Network {
     public static final String PROTOCOL_VERSION = "1";
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation(EtchedExtension.MODID, "main"),
+            ResourceLocation.fromNamespaceAndPath(EtchedExtension.MODID, "main"),
             () -> PROTOCOL_VERSION,
             PROTOCOL_VERSION::equals,
             PROTOCOL_VERSION::equals
